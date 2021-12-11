fbb89e8e003a9e2caa8219fd2ba3651d9aa689f3
Liu Rui <lovearuis@gmail.com>
2021-12-11 08:50:06 +0800

    改进一些文字

--- BookTime/View/About.swift
+++ BookTime/View/About.swift
@@ -18,8 +18,8 @@ struct About: View {
             Text("""
       创作这个App的💡来源于我正在上小学二年级的👧🏻，她有项作业是每天要阅读30分钟，有了这个App，我就能更好地督促她读书打卡了。
       老师每学期都要统计孩子的阅读量，要求家长做好孩子的阅读记录，所以这个App可帮了我大忙了，因为她每年的阅读量都在百本左右，我平时真的很难持续跟踪记录她的阅读进度。现在，我只要从App导出阅读数据就可以了。
-      这款App也帮我养成每天坚持阅读的习惯，你可以在这里找到我的[读书分享视频](https://space.bilibili.com/24370353/channel/seriesdetail?sid=320833)。也欢迎你发[邮件](mailto:cheetah_chugs.0g@icloud.com)给我，交流任何你想交流的事情。
-      最后，希望你喜欢这款App，也祝你永远享受阅读的乐趣❤️
+      这款App也帮我养成每天坚持阅读的习惯，您可以在这里找到我的[读书分享视频](https://space.bilibili.com/24370353/channel/seriesdetail?sid=320833)。也欢迎您发[邮件](mailto:cheetah_chugs.0g@icloud.com)给我，交流任何你想交流的事情。
+      最后，希望您喜欢这款App，也祝您永远享受阅读的乐趣❤️
 """)
             //                .font(.caption)
                 .lineSpacing(4)
@@ -33,8 +33,7 @@ struct About: View {
                         SKStoreReviewController.requestReview(in: scene)
                     }
                 }){
-                    Text("期待你的评价")
-                    Image(systemName: "face.smiling")
+                    Text("期待您的评价\(Image(systemName: "face.smiling"))")                    
                 }
             }.frame(minWidth:0,maxWidth: .infinity)
             
